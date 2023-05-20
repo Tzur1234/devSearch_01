535f2a9a32fdcc3bf0edda5d85b152565e088c14
root <root@DESKTOP-O1KC17T>
2023-05-20 20:33:03 +0300

    new changes

--- devsearch_01/utils/storages.py
+++ devsearch_01/utils/storages.py
@@ -3,9 +3,9 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticRootS3Boto3Storage(S3Boto3Storage):
     location = "static"
-    default_acl = "public-read"
+    # default_acl = "public-read"
 
 
 class MediaRootS3Boto3Storage(S3Boto3Storage):
     location = "media"
-    file_overwrite = False
+    # file_overwrite = False
