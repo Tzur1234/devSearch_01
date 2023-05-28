0fa987e4c82d80ccbb00ae13621c5f66e622f2ef
root <root@DESKTOP-O1KC17T>
2023-05-28 18:07:20 +0300

    male all fields in the form not required:

--- devsearch_01/core/forms.py
+++ devsearch_01/core/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class UserDetailForm(forms.Form):
-    name = forms.CharField(max_length=250, help_text="The name of the user", required=True)
+    name = forms.CharField(max_length=250, help_text="The name of the user", required=False)
     title = forms.CharField(max_length=250, help_text="Proffesional job's title", required=False)
     about = forms.CharField(widget=forms.Textarea(attrs={'rows':5}), required=False)
     profile_image = forms.ImageField(required=False)
