7f798229a856f97b74b0613ec85b5f41a4b6d2f8
root <root@DESKTOP-O1KC17T>
2023-05-29 23:46:33 +0300

    add The project Model and also add crop_size funtion

--- devsearch_01/core/admin.py
+++ devsearch_01/core/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
-from devsearch_01.core.models import Account, Skill
+from devsearch_01.core.models import Account, Skill, Tool, Image, Project
 
-admin.site.register(Account)
+
 admin.site.register(Skill)
+admin.site.register(Tool)
+
+
+# the intermediate table generated for the many-to-many relationship between Project and Image.
+class ImageInline(admin.TabularInline):
+    model = Project.image_project.through 
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):     
+    list_display = ('user', 'title', 'about', 'link', )
+    inlines = [ImageInline]
+    
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):     
+    list_display = ('user', 'name', 'title', 'about', 'profile_image', 'location', )
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):     
+    list_display = ('file', )
+    
--- devsearch_01/core/models.py
+++ devsearch_01/core/models.py
@@ -6,12 +6,50 @@ User = get_user_model()
 
 from django.db.models.signals import post_save
 
+# Pillow
+from PIL import Image as TheImage
+
 
 def get_image_location(instance, file_name):
     return f"account_profile_image/{instance.pk}/{file_name}"
 
 def get_default_image_path():
     return f"account_profile_image/default/default.png"
+
+
+def get_project_image_location(instance, file_name):
+    return f"profile_image/{instance.pk}/{file_name}"
+
+def get_default_project_image_path():
+    return f"project_image/default/default.png"
+
+def crop_image(image, size):
+    # Open the image using Pillow
+    img = TheImage.open(image)
+    
+    # Calculate the aspect ratio of the original image
+    aspect_ratio = img.width / img.height
+    
+    # Calculate the target width and height based on the desired size and aspect ratio
+    target_width = size[0]
+    target_height = int(target_width / aspect_ratio)
+    
+    # Resize the image maintaining the aspect ratio
+    img.thumbnail((target_width, target_height), TheImage.ANTIALIAS)
+    
+    # Calculate the cropping position based on the resized image dimensions
+    left = (img.width - size[0]) / 2
+    top = (img.height - size[1]) / 2
+    right = (img.width + size[0]) / 2
+    bottom = (img.height + size[1]) / 2
+    
+    # Crop the resized image
+    img = img.crop((left, top, right, bottom))
+    
+    # Save the cropped and resized image
+    img.save(image.path)
+
+
 
 
 class Account(models.Model):
@@ -22,12 +60,53 @@ class Account(models.Model):
     profile_image = models.ImageField(upload_to=get_image_location, default=get_default_image_path)
     location = models.CharField(max_length=250, null=True, blank=True)
 
+    def save(self, *args, **kwargs):
+        # Call the parent save() method to ensure other model operations are performed
+        super().save(*args, **kwargs)
+        
+        # Specify the desired size for cropping
+        crop_size = (600 , 600)  # Adjust as per your requirements
+        
+        # Call the crop_image method to crop the uploaded image
+        crop_image(self.profile_image, crop_size)
+
     def __str__(self) -> str:
         return f"Account -- > {self.user.username}"
 
 class Skill(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=250)
+
+class Tool(models.Model):
+    project = models.ForeignKey('Project', on_delete=models.CASCADE)
+    tool = models.CharField(max_length=250)
+
+class Image(models.Model):
+    file = models.ImageField(verbose_name='Project Image', upload_to=get_project_image_location, default=get_default_project_image_path)
+
+    def save(self, *args, **kwargs):
+        # Call the parent save() method to ensure other model operations are performed
+        super().save(*args, **kwargs)
+        
+        # Specify the desired size for cropping
+        crop_size = (1024, 768)  # Adjust as per your requirements
+        
+        # Call the crop_image method to crop the uploaded image
+        crop_image(self.file, crop_size)
+
+class Project(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image_project = models.ManyToManyField('Image', blank=True)
+    title = models.CharField(max_length=250, blank=True, null=True, default='Blank')
+    about = models.TextField(blank=True, null=True)
+    link = models.URLField(help_text="a link the deployed project", blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def save(self, *args, **kwargs):
+        if not self.url:  # If url field is empty
+            self.url = '/#/'  # Set a default URL
+        super().save(*args, **kwargs)
+
 
 
 # When the user created ...
